7cbd425d92aa4312e43ba1b0a6319ef5caa0e533
Paz Gueta <pazgueta@gmail.com>
2024-08-25 12:55:04 +0300

    connent to mysql

--- dist/index.js
+++ dist/index.js
@@ -8,12 +8,38 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
 const users_routes_1 = __importDefault(require("./routes/users.routes"));
+const mysql2_1 = require("mysql2");
 // import hotelsRoute from "./routes/hotels.routes";
 // import roomsRoute from "./routes/rooms.routes";
 const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
+const SQLPORT = process.env.SQLPORT || 20285;
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
+const connection = (0, mysql2_1.createConnection)({
+    host: process.env.AIVAN_HOST,
+    user: process.env.AIVAN_USERNAME,
+    password: process.env.AIVAN_PASSWORD,
+    database: process.env.AIVAN_DB,
+    connectTimeout: 30000,
+    port: 20285,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: process.env.CA_Certificate,
+    },
+});
+connection.connect((err) => {
+    if (err) {
+        console.error("Error connecting to the MySQL database:", err);
+    }
+    else {
+        console.log("Successfully connected to the MySQL database");
+    }
+    connection.end(); // Close the connection after testing
+});
+app.listen(SQLPORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 // Connect to MongoDB
 const connect = async () => {
     try {
